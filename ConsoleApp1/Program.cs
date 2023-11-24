e508758ebade9d438fcdf11f582af3bd50a9b11c
Zbiku <Zbiku@MSI>
2023-11-24 22:46:40 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿class zad
+{
+    static void Rysuj(int a, int b, char znak)
+    {
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                Console.Write(znak);
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main(string[] args)
+    {
+        int a, b;
+        char znak;
+        Console.WriteLine("Podaj wysokość twojej figury: ");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj długość twojej figury: ");
+        b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj znak wypełnienia twojej figury: ");
+        znak = char.Parse(Console.ReadLine());
+        Rysuj(a, b, znak);
+        Console.ReadLine();
+        Rysuj(b, a, znak);
+        Console.ReadKey();
+    }
+}
